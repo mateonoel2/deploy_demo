39053a16364e0ffd8854c23b7a403d4a8d3bfe5b
mateonoel2 <mateo.noel@utec.edu.pe>
2024-07-05 07:17:31 -0500

    a

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({
+  target: 'http://18.212.119.213:8080',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', // remove /api prefix when forwarding to the target
+  },
+  secure: false
+}));
+
+exports.apiProxy = functions.https.onRequest(app);
